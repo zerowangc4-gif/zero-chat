01fd5257469e67c4ac1d1458dd0cef5277624d42
wangc4 <1172524987@qq.com>
2025-12-20 16:08:41 +0800

    测试分支是否触发ci

--- App.tsx
+++ App.tsx
@@ -1,38 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from "@react-native/new-app-screen"
-import { StatusBar, StyleSheet, useColorScheme, View } from "react-native"
-import { SafeAreaProvider, useSafeAreaInsets } from "react-native-safe-area-context"
-
+import { Text } from "react-native"
+import { SafeAreaProvider } from "react-native-safe-area-context"
 function App() {
-  const isDarkMode = useColorScheme() === "dark"
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
-      <AppContent />
+      <Text>this is my app</Text>
     </SafeAreaProvider>
   )
 }
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets()
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen templateFileName="App.tsx" safeAreaInsets={safeAreaInsets} />
-    </View>
-  )
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-})
 
 export default App
