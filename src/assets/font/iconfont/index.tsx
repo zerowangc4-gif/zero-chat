384d5167435fb3afca51b548e8b3795909f744d1
wangc4 <1172524987@qq.com>
2025-12-29 02:11:55 +0800

    设计登录页

--- src/assets/font/iconfont/index.tsx
+++ src/assets/font/iconfont/index.tsx
@@ -4,14 +4,18 @@
 import React, { FunctionComponent } from 'react';
 import { ViewProps } from 'react-native';
 import { GProps } from 'react-native-svg';
+import IconDuihao from './IconDuihao';
+import IconDuihao1 from './IconDuihao1';
 import IconCloseCircle from './IconCloseCircle';
 import IconLaba from './IconLaba';
 import IconSousuo from './IconSousuo';
+export { default as IconDuihao } from './IconDuihao';
+export { default as IconDuihao1 } from './IconDuihao1';
 export { default as IconCloseCircle } from './IconCloseCircle';
 export { default as IconLaba } from './IconLaba';
 export { default as IconSousuo } from './IconSousuo';
 
-export type IconNames = 'close_circle' | 'laba' | 'sousuo';
+export type IconNames = 'duihao' | 'duihao1' | 'close_circle' | 'laba' | 'sousuo';
 
 interface Props extends GProps, ViewProps {
   name: IconNames;
@@ -21,12 +25,16 @@ interface Props extends GProps, ViewProps {
 
 let IconFont: FunctionComponent<Props> = ({ name, ...rest }) => {
   switch (name) {
+    case 'duihao':
+      return <IconDuihao key="1" {...rest} />;
+    case 'duihao1':
+      return <IconDuihao1 key="2" {...rest} />;
     case 'close_circle':
-      return <IconCloseCircle key="1" {...rest} />;
+      return <IconCloseCircle key="3" {...rest} />;
     case 'laba':
-      return <IconLaba key="2" {...rest} />;
+      return <IconLaba key="4" {...rest} />;
     case 'sousuo':
-      return <IconSousuo key="3" {...rest} />;
+      return <IconSousuo key="5" {...rest} />;
   }
 
   return null;
