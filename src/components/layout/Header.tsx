0d7b61bca6b30b5b697e4c06ee246cb4d3bba93a
wangc4 <1172524987@qq.com>
2026-02-10 06:29:20 +0800

    修复header背景色判断

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled, { useTheme } from "styled-components/native";
 import { Typography } from "../common/Typography";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-const HeaderContainer = styled.View<{ height: number; paddingTop: number; title?: string }>`
+const HeaderContainer = styled.View<{ height: number; paddingTop: number; hasContent: boolean }>`
   position: absolute;
   top: 0;
   left: 0;
@@ -12,7 +12,7 @@ const HeaderContainer = styled.View<{ height: number; paddingTop: number; title?
   padding-top: ${props => props.paddingTop}px;
   border-bottom-width: 1px;
   border-bottom-color: ${props => props.theme.colors.divider};
-  background-color: ${props => props.theme.colors.base};
+  background-color: ${props => (props.hasContent ? props.theme.colors.base : props.theme.colors.bgPage)};
 `;
 const HeaderContent = styled.View`
   flex: 1;
@@ -44,8 +44,9 @@ export const Header: React.FC<HeaderProps> = ({ title, leftElement, rightElement
   const theme = useTheme();
   const insets = useSafeAreaInsets();
   const height = theme.spacing.layout.navBarHeight + insets.top;
+  const hasContent = title || leftElement || rightElement;
   return (
-    <HeaderContainer height={height} paddingTop={insets.top} title={title}>
+    <HeaderContainer height={height} paddingTop={insets.top} hasContent={hasContent}>
       <HeaderContent>
         <SideSlot $align="left">{leftElement}</SideSlot>
         <NavTitle numberOfLines={1}>{title}</NavTitle>
